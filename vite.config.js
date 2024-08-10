9e7d4bea8a284375d034387559b5cc2d47726199
Ankit Akshdeep <ankit.akshdeep25@gmail.com>
2024-08-10 21:41:20 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
+  build: {
+    outDir: "dist", // This is the default, but make sure it's set correctly
+  },
 });
